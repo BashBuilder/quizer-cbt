9d84e0e047e2f152dd3f8e1f25c60d8cabd06b87
unknown <iamadelowotan@gmail.com>
2025-02-02 16:42:51 +0100

    Update middleware to redirect to /auth instead of /login

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,7 @@ import type { NextRequest } from "next/server";
 export async function middleware(request: NextRequest) {
   const token = request.cookies.get("auth") || "";
   if (!token) {
-    return NextResponse.redirect(new URL("/login", request.url));
+    return NextResponse.redirect(new URL("/auth", request.url));
   }
   if (request.url.includes("/auth")) {
     return NextResponse.redirect(new URL("/dashboard", request.url));

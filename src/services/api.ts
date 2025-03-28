708dcd622481ec789feaf4f78a87a653f706cac4
unknown <iamadelowotan@gmail.com>
2025-03-28 08:15:58 +0100

    update: add functions to retrieve ordered English questions and refactor question retrieval logic

--- src/services/api.ts
+++ src/services/api.ts
@@ -35,6 +35,58 @@ export const getRandomQuestionsBySubject = async (
     );
   }
 };
+export const getOrderedQuestionsBySubject = async (
+  subject: string,
+  number: number,
+) => {
+  try {
+    const randomYear = Math.floor(Math.random() * 8 + 2003);
+    const url =
+      process.env.NEXT_PUBLIC_ALOC_BASE_URL +
+      `m/${number}?subject=${subject}&year=${randomYear}&random=false`;
+
+    const response = await axios.get<{
+      subject: string;
+      status: number;
+      data: QuestionType[];
+    }>(url, {
+      headers: {
+        "Content-Type": "application/json",
+        AccessToken: process.env.NEXT_PUBLIC_ALOC_TOKEN,
+        Accept: "application/json",
+      },
+    });
+
+    return response.data;
+  } catch (error) {
+    if (error) {
+      throw error;
+    }
+    throw new Error(
+      `Failed to fetch questions for subject: ${subject} ${
+        (error as Error).message
+      }`,
+    );
+  }
+};
+export const getEnglishQuestionFromDb = async () => {
+  try {
+    const { data } = await axios.get(
+      process.env.NEXT_PUBLIC_API_BASE_URL + "question/english",
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${getCookie(userStore.token)}`,
+        },
+      },
+    );
+    return data;
+  } catch (error) {
+    console.log(error);
+    throw new Error("failed to fetch english questions");
+  }
+};
+
 export const getQuestions = async (data: {
   subjects: string[];
   jamb?: boolean;
@@ -50,25 +102,32 @@ export const getQuestions = async (data: {
 
     for (const subject of subjects) {
       if (subject === "english") {
-        const { data } = await axios.post<{
-          subject: string;
-          status: number;
-          data: QuestionType[];
-        }>(
-          process.env.NEXT_PUBLIC_API_BASE_URL + "question/random",
-          {
-            subject: "english",
-            number,
-            jamb,
-          },
-          {
-            headers: {
-              "Content-Type": "application/json",
-              Authorization: `Bearer ${getCookie(userStore.token)}`,
-            },
-          },
-        );
-        questions.push(data);
+        // const { data } = await axios.post<{
+        //   subject: string;
+        //   status: number;
+        //   data: QuestionType[];
+        // }>(
+        //   process.env.NEXT_PUBLIC_API_BASE_URL + "question/random",
+        //   {
+        //     subject: "english",
+        //     number,
+        //     jamb,
+        //   },
+        //   {
+        //     headers: {
+        //       "Content-Type": "application/json",
+        //       Authorization: `Bearer ${getCookie(userStore.token)}`,
+        //     },
+        //   },
+        // );
+        const part1 = await getEnglishQuestionFromDb();
+        const part2 = await getOrderedQuestionsBySubject("english", 20);
+        const q = {
+          subject: "english",
+          status: 200,
+          data: [...part1.data, ...part2.data],
+        };
+        questions.push(q);
       } else {
         await getRandomQuestionsBySubject(subject, number).then((data) =>
           questions.push(data),
@@ -79,8 +138,7 @@ export const getQuestions = async (data: {
     const response = await axios.post<LoginResponse>(
       process.env.NEXT_PUBLIC_API_BASE_URL + "user/me",
       {
-        subject: "english",
-        number,
+        updateCount: true,
         jamb,
       },
       {

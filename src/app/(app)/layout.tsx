583f7f4152355cfd152be2e0d2d44d54ac97f01b
unknown <iamadelowotan@gmail.com>
2025-06-23 20:40:52 +0100

    update added some comment

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -2,6 +2,12 @@ import Navbar from "@/components/layout/navbar";
 import { userStore } from "@/data/constants";
 import { cookies } from "next/headers";
 
+// added some comments on this page
+// added some comments on this page
+// added some comments on this page
+// added some comments on this page
+// added some comments on this page
+
 export default async function RootLayout({
   children,
 }: Readonly<{

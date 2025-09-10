503d67b9e3165c7d5036df614f7ec54794741cd6
unknown <iamadelowotan@gmail.com>
2025-09-10 11:49:18 +0100

    update temporary remove cookie validation

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,18 +1,17 @@
 import Navbar from "@/components/layout/navbar";
-import { userStore } from "@/data/constants";
-import { cookies } from "next/headers";
-
+// import { userStore } from "@/data/constants";
+// import { cookies } from "next/headers";
 
 export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const cookie = (await cookies()).get(userStore.token)?.value;
+  // const cookie = (await cookies()).get(userStore.token)?.value;
 
-  if (!cookie) {
-    window.location.href = "/";
-  }
+  // if (!cookie) {
+  //   window.location.href = "/";
+  // }
 
   return (
     <html lang="en">

0607d0866f2fe112e3638cd59a06b809dd8604ac
unknown <iamadelowotan@gmail.com>
2025-09-24 21:26:33 +0100

    update added some comments

--- src/app/cookie/page.tsx
+++ src/app/cookie/page.tsx
@@ -284,6 +284,7 @@ export default function CookiePolicyPage() {
             </CardContent>
           </Card>
 
+          {/* added some other comments */}
           {/* Managing Cookies */}
           <Card>
             <CardHeader>

8737ecec8ae6fe8c375a6f9c4218579b3ad1c751
unknown <iamadelowotan@gmail.com>
2025-12-15 18:22:22 +0100

    modified application

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,17 +1,7 @@
-// /** @type {import('postcss-load-config').Config} */
-// const config = {
-//   plugins: {
-//     tailwindcss: {},
-//     autoprefixer: {},
-//   },
-// };
-
-// export default config;
-
-// postcss.config.js
-export default {
+const config = {
   plugins: {
     "@tailwindcss/postcss": {},
-    autoprefixer: {},
   },
 };
+
+export default config;

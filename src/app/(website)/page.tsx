af617e25b7e27dc7a9b7fa7bf3963c31f8e81041
unknown <iamadelowotan@gmail.com>
2026-01-01 20:08:06 +0100

    update: clean up the landing page

--- src/app/(website)/page.tsx
+++ src/app/(website)/page.tsx
@@ -1,16 +1,12 @@
 "use client";
 
-// import WelcomePopUp from "@/components/popups/welcome-pop";
 import About from "@/components/home/about";
 import Landing from "@/components/home/landing";
-// import Subjects from "@/components/home/subjects";
 
 export default function Home() {
   return (
     <main className="bg-slate-50">
-      {/* <WelcomePopUp /> */}
       <Landing />
-      {/* <Subjects /> */}
       <About />
     </main>
   );

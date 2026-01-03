305a1ac04ab55984edf425c1120155180d392e4e
unknown <iamadelowotan@gmail.com>
2026-01-03 17:08:36 +0100

    update: cleanup config

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,13 +1,3 @@
-// /** @type {import('postcss-load-config').Config} */
-// const config = {
-//   plugins: {
-//     tailwindcss: {},
-//     autoprefixer: {},
-//   },
-// };
-
-// export default config;
-
 // postcss.config.js
 export default {
   plugins: {
